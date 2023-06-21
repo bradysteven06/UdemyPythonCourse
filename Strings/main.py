e549f8cdf161ed134504847024df12aa5367068a
Steven Brady <bradys@my.lanecc.edu>
2023-06-21 09:30:20 -0700

    working on strings review

---
+++ Strings/main.py
@@ -0,0 +1,29 @@
+
+
+
+if __name__ == '__main__':
+
+    string1 = 'hello'
+    string2 = "world"
+    string3 = 'this is also a string'
+    string4 = "I'm going for a run"
+    print("hello")
+    print(type(string1))
+    print('hello \nworld')
+    print(len(string1))
+
+    my_string = "Hello World"
+    print(my_string[0])
+    print(my_string[8])
+    print(my_string[-2])
+
+# my_string[start:stop:step]
+    my_string = 'abcdefghijk'
+    print(my_string[2:])
+    print(my_string[:3])
+    print(my_string[3:6])
+    print(my_string[1:3])
+    print(my_string[7:10])
+    print(my_string[::2])
+    print('Hello World'[8])
+
---
+++ Variables/main.py
@@ -0,0 +1,21 @@
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Variable assignment and printing to console
+    a = 5
+    print(a)
+    a = 10
+    print(a)
+    print(a+a)
+
+    a = a + a
+    print(a)
+
+    print(type(a))
+
+    my_income = 100
+    tax_rate = 0.1
+    my_taxes = my_income * tax_rate
+    print(my_taxes)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
