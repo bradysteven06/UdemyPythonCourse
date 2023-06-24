86f66131718cc767404a44a8d5694928f29589e3
Steven Brady <bradys@my.lanecc.edu>
2023-06-24 07:18:34 -0700

    finished if statements video

---
+++ Section5PythonStatements/IfStatements.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+
+    loc = "Bank"
+
+    if loc == "Auto Shop":
+        print("Cars are cool!")
+    elif loc == "Bank":
+        print("Money is cool!")
+    elif loc == "Store":
+        print("Welcome to the store!")
+    else:
+        print("I don't know much.")
+
+    name = "Sammy"
+
+    if name == "Frankie":
+        print("Hello Frankie!")
+    elif name == "Sammy":
+        print("Hello Sammy!")
+    else:
+        print("What is your name")
