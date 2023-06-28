202fcd08463f9043f2f25e75ab0683c972c7e8c3
Steven Brady <bradys@my.lanecc.edu>
2023-06-27 23:56:41 -0700

    finished tuple unpacking with functions lecture

--- Section6MethodsAndFunctions/functions.py
+++ Section6MethodsAndFunctions/functions.py
@@ -25,6 +25,21 @@ def check_even_list(num_list):
     return even_numbers
 
 
+def employee_check(work_hours):
+    current_max = 0
+    employee_of_month = ""
+
+    for employee, hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+
+    return (employee_of_month, current_max)
+
+
+
 if __name__ == '__main__':
 
     say_hello("kevin")
@@ -32,4 +47,8 @@ if __name__ == '__main__':
     print(result)
     print(check_even_list([1, 2, 3, 4, 5]))
 
-
+    work_hours = [("Abby", 100), ("Billy", 850), ("Cassie", 800)]
+    print(employee_check(work_hours))
+    name, hours = employee_check(work_hours)
+    print(name)
+    print(hours)
