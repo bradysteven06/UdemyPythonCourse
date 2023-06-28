bc99040a77e6f89d9185ff9f3ecdf1dae8791dc0
Steven Brady <bradys@my.lanecc.edu>
2023-06-27 23:43:43 -0700

    started section 6 - Methods and Functions

---
+++ Section6MethodsAndFunctions/functions.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+
+    def say_hello(name):
+        print(f"Hello {name}")
+
+    def add_num(num1, num2):
+        return num1 + num2
+
+    def return_result(a, b):
+        return a + b
+
+    def even_check(number):
+        return number % 2 == 0
+
+    def check_even_list(num_list):
+        even_numbers = []
+
+        for number in num_list:
+            if number % 2 == 0:
+                even_numbers.append(number)
+            else:
+                pass
+        return even_numbers
+
+
+    say_hello("kevin")
+    result = return_result(5, 9)
+    print(result)
+    print(check_even_list([1, 2, 3, 4, 5]))
