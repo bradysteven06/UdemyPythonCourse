e08c8d01f2dffcd6ae9bae2eb58f5b9fb73cb2a7
Steven Brady <bradys@my.lanecc.edu>
2023-06-29 15:49:37 -0700

    finished level 2 problems

--- Section6MethodsAndFunctions/practice_exercises.py
+++ Section6MethodsAndFunctions/practice_exercises.py
@@ -37,6 +37,73 @@ def old_macdonald(name):
     return "".join(mylist)
 
 
+def master_yoda(sentence):
+    reverse_list = []
+    for word in reversed(sentence.split()):
+        reverse_list.append(word)
+    return " ".join(reverse_list)
+
+
+def almost_there(n):
+    if abs(n - 100) <= 10 or abs(n - 200) <= 10:
+        return True
+    else:
+        return False
+
+
+def has_33(list_ints):
+    first_three = False
+    second_three = False
+    for num in list_ints:
+        if num == 3 and not first_three:
+            first_three = True
+        elif num != 3 and first_three:
+            first_three = False
+        elif num == 3 and first_three:
+            second_three = True
+
+    if first_three and second_three:
+        return True
+    else:
+        return False
+
+
+def paper_doll(my_string):
+    characters = []
+    for char in my_string:
+        characters.append(char*3)
+    return "".join(characters)
+
+
+def blackjack(num1, num2, num3):
+    sum_numbers = num1 + num2 + num3
+    if sum_numbers <= 21:
+        return sum_numbers
+    elif sum_numbers > 21 and num1 == 11 or num2 == 11 or num3 == 11:
+        sum_numbers -= 10
+        if sum_numbers <= 21:
+            return sum_numbers
+        else:
+            return "BUST"
+    else:
+        return "BUST"
+
+
+def summer_69(num_list):
+    sum_total = 0
+    six_rule_active = False
+    for num in num_list:
+        if num == 6:
+            six_rule_active = True
+        elif num == 9:
+            six_rule_active = False
+        elif six_rule_active:
+            pass
+        else:
+            sum_total += num
+    return sum_total
+
+
 if __name__ == '__main__':
 
     print(old_macdonald("macdonald"))
