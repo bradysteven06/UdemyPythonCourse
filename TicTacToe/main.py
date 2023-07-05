4959e4d63c0ed9265f470b2ee10e2afa66a185ca
Steven Brady <bradys@my.lanecc.edu>
2023-07-04 22:02:33 -0700

    started tic tac toe project

---
+++ TicTacToe/main.py
@@ -0,0 +1,31 @@
+
+def display_instructions():
+    print(f"Instructions")
+
+
+def display_board():
+    print(f"Board")
+
+
+def create_board():
+    board = []
+    return board
+
+
+def get_move():
+    move = input("insert move")
+    return move
+
+
+def make_move():
+    valid_move = get_move()
+
+
+def check_win():
+    is_winner = False
+
+
+if __name__ == '__main__':
+
+    display_instructions()
+
