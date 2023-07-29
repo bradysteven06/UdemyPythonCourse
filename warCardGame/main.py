3bf358a74d15dcb1caf84e0c12617f7676d23a6e
Steven Brady <bradys@my.lanecc.edu>
2023-07-29 04:39:39 -0700

    tested card, deck, and player classes

--- warCardGame/main.py
+++ warCardGame/main.py
@@ -1,4 +1,5 @@
 from deck import Deck
+from player import Player
 
 if __name__ == '__main__':
 
@@ -10,4 +11,8 @@ if __name__ == '__main__':
     my_card = my_deck.deal_one()
     print(my_card)
     print(len(my_deck.all_cards))
-
+    steven = Player("Steven")
+    print(steven)
+    steven.add_cards(my_card)
+    print(steven)
+    print(steven.all_cards[0])
