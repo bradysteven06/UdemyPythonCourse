e2b5e2d6cabf132cf81e88c3a018bee9ff51c51f
Steven Brady <bradys@my.lanecc.edu>
2023-07-30 06:08:59 -0700

    modified warCardGame to not use break statements

--- warCardGame/main.py
+++ warCardGame/main.py
@@ -37,68 +37,53 @@ if __name__ == '__main__':
         print(f"Round {round_count}")
 
         # Check to see if a player is out of cards:
-        if out_of_cards_check(player_one.name, player_one.all_cards):
-            game_over = True
-            break
+        game_over = out_of_cards_check(player_one.name, player_one.all_cards) or out_of_cards_check(player_two.name,
+                                                                                                    player_two.all_cards)
 
-        if out_of_cards_check(player_two.name, player_two.all_cards):
-            game_over = True
-            break
+        # Game is still on
+        if not game_over:
 
-        # Otherwise, the game is still on!
+            # Start a new round and reset current cards "on the table"
+            player_one_cards = [player_one.remove_one()]
+            player_two_cards = [player_two.remove_one()]
 
-        # Start a new round and reset current cards "on the table"
-        player_one_cards = []
-        player_one_cards.append(player_one.remove_one())
+            at_war = player_one_cards[-1].value == player_two_cards[-1].value
 
-        player_two_cards = []
-        player_two_cards.append(player_two.remove_one())
+            while at_war and not game_over:
 
-        at_war = True
-
-        while at_war:
-
-            if player_one_cards[-1].value > player_two_cards[-1].value:
-
-                # Player One gets the cards
-                player_one.add_cards(player_one_cards)
-                player_one.add_cards(player_two_cards)
-
-                # No Longer at "war" , time for next round
-                at_war = False
-
-            # Player Two Has higher Card
-            elif player_one_cards[-1].value < player_two_cards[-1].value:
-
-                # Player Two gets the cards
-                player_two.add_cards(player_one_cards)
-                player_two.add_cards(player_two_cards)
-
-                # No Longer at "war" , time for next round
-                at_war = False
-
-            else:
                 print('WAR!')
+
                 # This occurs when the cards are equal.
                 # We'll grab another card each and continue the current war.
-
-                # First check to see if player has enough cards
 
                 # Check to see if a player is out of cards:
                 if len(player_one.all_cards) < 5:
                     print("Player One unable to play war! Game Over at War")
                     print("Player Two Wins! Player One Loses!")
-                    game_on = False
-                    break
+                    game_over = True
 
                 elif len(player_two.all_cards) < 5:
                     print("Player Two unable to play war! Game Over at War")
                     print("Player One Wins! Player Two Loses!")
-                    game_on = False
-                    break
+                    game_over = True
+
                 # Otherwise, we're still at war, so we'll add the next cards
                 else:
                     for num in range(5):
                         player_one_cards.append(player_one.remove_one())
                         player_two_cards.append(player_two.remove_one())
+
+                # Check for war condition again
+                at_war = player_one_cards[-1].value == player_two_cards[-1].value
+
+            if not game_over:
+                if player_one_cards[-1].value > player_two_cards[-1].value:
+                    # Player One gets the cards
+                    player_one.add_cards(player_one_cards)
+                    player_one.add_cards(player_two_cards)
+
+                elif player_one_cards[-1].value < player_two_cards[-1].value:
+                    # Player Two gets the cards
+                    player_two.add_cards(player_one_cards)
+                    player_two.add_cards(player_two_cards)
 
